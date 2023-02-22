1f068e741420e17523fbaeeb32b8b22d264596c5
Kazuo-hagy <kazuo@affinityseguro.com.br>
2023-02-22 20:37:49 -0300

    tela login

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Image, TextInput, Button } from 'react-native';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Meu primeiro app rodando no expo :D</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
+      <Image source={require('./assets/gaia.png')} style={styles.Image}></Image>
+      <Text style={styles.Label}>GAIA</Text>
+      <TextInput style={styles.Input} placeholder="Email" />
+      <TextInput  style={styles.Input}placeholder="Senha" secureTextEntry />
+      <Button style={styles.mario} title="Entrar" color="#2F4F4F" />
+      <Text style={styles.Label1}>Não tem uma conta?</Text>
+      <Button style={styles.Button} title="Cadastrar" color="#2F4F4F" />
     </View>
   );
 }
@@ -13,8 +20,36 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#3CB371',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  Image:{
+    width: 112,
+    height: 100,
+  },
+  Label:{
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  Label1:{
+    color: 'white',
+    fontSize: 10,
+    fontWeight: 'bold',
+    marginTop: 10,
+  },
+  Input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    backgroundColor:'#fff',
+    width: 300,
+  },
+  mario:{
+    margin: 10,
+    color: '#3CB37',
+    padding: 10,
+  }
 });
