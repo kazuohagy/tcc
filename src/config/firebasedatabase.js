6254fa84e1a6cfc98ff543c98d2a190ba928c568
kazuohagy <kazuohagy@users.noreply.github.com>
2023-03-23 08:20:38 -0300

    Working database :fire:

---
+++ src/config/firebasedatabase.js
@@ -0,0 +1,19 @@
+import {initializeApp} from "firebase/app";
+import {getFirestore} from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCjgPe0vmY7thIgBubGp45BflEzsq98CYA",
+    authDomain: "gaia-25c62.firebaseapp.com",
+    projectId: "gaia-25c62",
+    storageBucket: "gaia-25c62.appspot.com",
+    messagingSenderId: "1018019238870",
+    appId: "1:1018019238870:web:ec173febc950f233764a77",
+    measurementId: "G-YRBPCCYHHC"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export default db;
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -3,11 +3,36 @@ import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
-
+import db from '../config/firebasedatabase';
+import { useEffect, useState } from 'react';
+import { collection, getDocs } from "firebase/firestore";
 function Feed() {
+  const [plants, setPlants] = useState([]);
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      const querySnapshot = await getDocs(collection(db, "Plants"));
+      const list = [];
+      querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} => ${doc.data()}`);
+        list.push({...doc.data(), id: doc.id});
+      });
+      setPlants(list);
+    };
+    fetchData();
+  }, []);
+  console.log("ESSAS SAO AS PLANTAS DESGRACA")
+  console.log(plants);
+
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Feed!</Text>
+        {plants.map((plant) => (
+          <View key={plant.id}>
+          <Text>{plant.name}</Text>
+          <Text>{plant.description}</Text>
+          </View>
+        ))}
     </View>
   );
 }
@@ -43,6 +68,7 @@ export default function Home() {
         component={Feed}
         options={{
           tabBarLabel: 'Home',
+          headerTintColor: '#3CB371',
           tabBarIcon: ({ color, size }) => (
             <MaterialCommunityIcons name="home" color={color} size={size} />
           ),
@@ -53,6 +79,7 @@ export default function Home() {
         component={Notifications}
         options={{
           tabBarLabel: 'Updates',
+          headerTintColor: '#3CB371',
           tabBarIcon: ({ color, size }) => (
             <MaterialCommunityIcons name="bell" color={color} size={size} />
           ),
@@ -63,6 +90,7 @@ export default function Home() {
         component={Profile}
         options={{
           tabBarLabel: 'Profile',
+          headerTintColor: '#3CB371',
           tabBarIcon: ({ color, size }) => (
             <MaterialCommunityIcons name="account" color={color} size={size} />
           ),
