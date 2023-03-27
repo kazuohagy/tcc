66d374eaf7ce819652f39fa466401a10eae75564
kazuohagy <kazuohagy@users.noreply.github.com>
2023-03-26 21:01:34 -0300

    [AppRoutes.js] add Route NewPlants

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -1,21 +1,29 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Login from '../screens/Login';
-import Cadastro from '../screens/Cadastro';
-import Home from '../screens/Home';
-import NewPlant from '../screens/NewPlant';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Login from "../screens/Login";
+import Cadastro from "../screens/Cadastro";
+import Home from "../screens/Home";
+import NewPlant from "../screens/NewPlant";
 
 const Stack = createNativeStackNavigator();
 export default function AppRoutes() {
-    return (
-        <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Login" component={Login} />
-          <Stack.Screen name="Cadastro" component={Cadastro} />
-          <Stack.Screen name="Home" component={Home} options={{ headerShown: false }}  />
-          <Stack.Screen name="NewPlant" component={NewPlant}  />
-        </Stack.Navigator>
-      </NavigationContainer>
-    );
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Cadastro" component={Cadastro} />
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="NewPlant"
+          component={NewPlant}
+          options={{ headerTintColor: "#3CB371" }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
