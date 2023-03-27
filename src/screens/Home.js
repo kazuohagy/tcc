71d309cc33423bbcd8cfcf0befb91ef8567fb213
kazuohagy <kazuohagy@users.noreply.github.com>
2023-03-26 21:01:55 -0300

    [Home.js] [NewPlant.js] add page

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,20 +1,25 @@
-import * as React from 'react';
-import { FlatList, Text, TouchableOpacity, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
-import db from '../config/firebasedatabase';
-import { useEffect, useState } from 'react';
+import * as React from "react";
+import { FlatList, Text, TouchableOpacity, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import db from "../config/firebasedatabase";
+import { useEffect, useState } from "react";
 import { collection, getDocs } from "firebase/firestore";
-import { StyleSheet } from 'react-native';
-function Feed({navigation}) {
+import { StyleSheet, Image } from "react-native";
+import { useFocusEffect } from "@react-navigation/native";
+function Feed({ navigation }) {
   const [plants, setPlants] = useState([]);
   function deletePlant(id) {
-    db.collection("Plants").doc(id).delete().then(() => {
-      console.log("Document successfully deleted!");
-    }).catch((error) => {
-      console.error("Error removing document: ", error);
-    });
+    db.collection("Plants")
+      .doc(id)
+      .delete()
+      .then(() => {
+        console.log("Document successfully deleted!");
+      })
+      .catch((error) => {
+        console.error("Error removing document: ", error);
+      });
   }
   useEffect(() => {
     const fetchData = async () => {
@@ -22,81 +27,103 @@ function Feed({navigation}) {
       const list = [];
       querySnapshot.forEach((doc) => {
         console.log(`${doc.id} => ${doc.data()}`);
-        list.push({...doc.data(), id: doc.id});
+        list.push({ ...doc.data(), id: doc.id });
       });
       setPlants(list);
     };
     fetchData();
   }, []);
-  console.log("ESSAS SAO AS PLANTAS DESGRACA")
+  useFocusEffect(
+    React.useCallback(() => {
+      const fetchData = async () => {
+        const querySnapshot = await getDocs(collection(db, "Plants"));
+        const list = [];
+        querySnapshot.forEach((doc) => {
+          console.log(`${doc.id} => ${doc.data()}`);
+          list.push({ ...doc.data(), id: doc.id });
+        });
+        setPlants(list);
+      };
+      fetchData();
+    }, [])
+  );
+  console.log("ESSAS SAO AS PLANTAS DESGRACA");
   console.log(plants);
 
   return (
     <View style={styles.container}>
-
       <FlatList
-      showsVerticalScrollIndicator={false}
-      data={plants}
-      renderItem={({ item }) => (
-        <View style={styles.buttonContainer}>
-          <TouchableOpacity style={styles.buttonNewPlant} onPress={()=>navigation.navigate("EditPlant")}>
-            <Text style={{color:'#3CB371'}}>{item.name}</Text>
-            <Text >{item.description}</Text>
-          </TouchableOpacity>
-        </View>
-      )}
+        showsVerticalScrollIndicator={false}
+        data={plants}
+        renderItem={({ item }) => (
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity
+              style={styles.buttonNewPlant}
+              onPress={() => navigation.navigate("EditPlant")}
+            >
+              <Text style={{ color: "#3CB371" }}>{item.name}</Text>
+              {item.image && (
+                <Image
+                  source={{ uri: item.image }}
+                  style={{ width: 200, height: 200 }}
+                />
+              )}
+              <Text>{item.description}</Text>
+            </TouchableOpacity>
+          </View>
+        )}
       />
-       <TouchableOpacity onPress={()=>navigation.navigate("NewPlant")}>
-          <Text style={styles.iconButton}>Adicionar planta</Text>
-       </TouchableOpacity>
+      <TouchableOpacity onPress={() => navigation.navigate("NewPlant")}>
+        <Text style={styles.iconButton}>Adicionar planta</Text>
+      </TouchableOpacity>
     </View>
   );
 }
 const styles = StyleSheet.create({
   container: {
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
   },
   buttonContainer: {
-      marginTop: 20,
-      width: 300,
-      borderRadius: 20,
-      overflow: 'hidden',
+    marginTop: 20,
+    width: 300,
+    borderRadius: 20,
+    overflow: "hidden",
   },
   iconButton: {
-    color: '#3CB371',
+    color: "#3CB371",
     fontSize: 20,
-    fontWeight: 'bold',
-    textAlign: 'center',
+    fontWeight: "bold",
+    textAlign: "center",
     padding: 10,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     borderRadius: 10,
     width: 200,
     marginTop: 20,
   },
   buttonNewPlant: {
-    color: '#3CB371',
+    color: "#3CB371",
     fontSize: 20,
-    fontWeight: 'bold',
-    textAlign: 'center',
+    fontWeight: "bold",
+    textAlign: "center",
     padding: 10,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     borderRadius: 10,
-    width: '100%',
+    width: "100%",
     marginTop: 20,
   },
   caixa: {
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     borderRadius: 10,
-    width: '100%',
+    width: "100%",
     marginTop: 20,
-  }
+  },
 });
 
 function Profile() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Profile!</Text>
     </View>
   );
@@ -104,7 +131,7 @@ function Profile() {
 
 function Notifications() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Notifications!</Text>
     </View>
   );
@@ -117,15 +144,15 @@ export default function Home() {
     <Tab.Navigator
       initialRouteName="Feed"
       screenOptions={{
-        tabBarActiveTintColor: '#3CB371',
+        tabBarActiveTintColor: "#3CB371",
       }}
     >
       <Tab.Screen
         name="Feed"
         component={Feed}
         options={{
-          tabBarLabel: 'Home',
-          headerTintColor: '#3CB371',
+          tabBarLabel: "Home",
+          headerTintColor: "#3CB371",
           tabBarIcon: ({ color, size }) => (
             <MaterialCommunityIcons name="home" color={color} size={size} />
           ),
@@ -135,8 +162,8 @@ export default function Home() {
         name="Notifications"
         component={Notifications}
         options={{
-          tabBarLabel: 'Updates',
-          headerTintColor: '#3CB371',
+          tabBarLabel: "Updates",
+          headerTintColor: "#3CB371",
           tabBarIcon: ({ color, size }) => (
             <MaterialCommunityIcons name="bell" color={color} size={size} />
           ),
@@ -146,8 +173,8 @@ export default function Home() {
         name="Profile"
         component={Profile}
         options={{
-          tabBarLabel: 'Profile',
-          headerTintColor: '#3CB371',
+          tabBarLabel: "Profile",
+          headerTintColor: "#3CB371",
           tabBarIcon: ({ color, size }) => (
             <MaterialCommunityIcons name="account" color={color} size={size} />
           ),
@@ -156,6 +183,3 @@ export default function Home() {
     </Tab.Navigator>
   );
 }
-
-
-
--- src/screens/NewPlant.js
+++ src/screens/NewPlant.js
@@ -1,59 +1,209 @@
-import React from 'react';
-import {StyleSheet, Text,Button, View} from 'react-native';
-import InputImage from '../components/InputImage';
-import { useState } from 'react';
-import { createUserWithEmailAndPassword } from "firebase/auth";
-import { auth } from '../config/firebaseconfig';
-import db from '../config/firebasedatabase';
+import React, { useEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  Button,
+  View,
+  ScrollView,
+  Image,
+} from "react-native";
+import InputImage from "../components/InputImage";
+import { useState } from "react";
+import db from "../config/firebasedatabase";
 import { collection, addDoc } from "firebase/firestore";
-export default function NewPlant({navigation}){
-    const [name, setName] = useState('');
-    const [description, setDescription] = useState('');
-    const [message, setMessage] = useState('');
-    const [error, setError] = useState(false);
+import InputImageDescription from "../components/InputImageDescription";
+import * as ImagePicker from "expo-image-picker";
+import { storage } from "../config/firebasedatabase";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 
-    const addPlant = async () => {
-        try {
-            const docRef = await addDoc(collection(db, "Plants"), {
-              name: name,
-              description: description,
-            });
-            console.log("Document written with ID: ", docRef.id);
-          } catch (e) {
-            console.error("Error adding document: ", e);
-            setError(true);
-            setMessage('Erro ao cadastrar planta');
-          }
+export default function NewPlant({ navigation }) {
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [message, setMessage] = useState("");
+  const [error, setError] = useState(false);
+  const [image, setImage] = useState(null);
+  const [imageURL, setImageURL] = useState("");
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage(result.uri);
     }
+  };
+  const addPlant = async () => {
+    try {
+      const docRef = await addDoc(collection(db, "Plants"), {
+        name: name,
+        description: description,
+        image: imageURL,
+      });
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+      setError(true);
+      setMessage("Erro ao cadastrar planta");
+    }
+  };
+  //useEffect(() => {
+  //    (async () => {
+  //        if (Platform.OS !== 'web') {
+  //            const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+  //            if (status !== 'granted') {
+  //                alert('Sorry, we need camera roll permissions to make this work!');
+  //            }
+  //        }
+  //    })();
+  //}, []);
+  useEffect(() => {
+    const uploadImage = async () => {
+      //aqui faz a conversao da imagem para blob
+      const blob = await new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.onload = function () {
+          resolve(xhr.response);
+        };
+        xhr.onerror = function (e) {
+          console.log(e);
+          reject(new TypeError("Network request failed"));
+        };
+        xhr.responseType = "blob";
+        xhr.open("GET", image, true);
+        xhr.send(null);
+      });
+      // Create the file metadata
+      /** @type {any} */
+      const metadata = {
+        contentType: "image/jpeg",
+      };
+      // Upload file and metadata to the object 'images/mountains.jpg'
+      const storageRef = ref(storage, "Plants/" + Date.now() + ".jpg");
+      const uploadTask = uploadBytesResumable(storageRef, blob, metadata);
 
-    return(
-        <View style={styles.container}>
-            {error === true && <Text style={styles.error}>{message}</Text>}
-             <InputImage placeholder="Nome" imageName="leaf" onChangeText={text => setName(text)} value={name}  />
-             <InputImage placeholder="Descricao" imageName="leaf" onChangeText={text => setDescription(text)} value={description} />
-            <View style={styles.buttonContainer}>
-            <Button title='Cadastrar'color="#2F4F4F" style={styles.byton} onPress={()=>addPlant() && navigation.navigate("Home") }>Cadastrar</Button>
-            </View>
-            <View style={styles.buttonContainer}>
-            <Button title='Voltar'color="#2F4F4F" style={styles.byton} onPress={()=>props.navigation.navigate('Login')}>Voltar</Button>
-            </View>
+      // Listen for state changes, errors, and completion of the upload.
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log("Upload is " + progress + "% done");
+          switch (snapshot.state) {
+            case "paused":
+              console.log("Upload is paused");
+              break;
+            case "running":
+              console.log("Upload is running");
+              break;
+          }
+        },
+        (error) => {
+          // A full list of error codes is available at
+          // https://firebase.google.com/docs/storage/web/handle-errors
+          switch (error.code) {
+            case "storage/unauthorized":
+              // User doesn't have permission to access the object
+              break;
+            case "storage/canceled":
+              // User canceled the upload
+              break;
+
+            // ...
+
+            case "storage/unknown":
+              // Unknown error occurred, inspect error.serverResponse
+              break;
+          }
+        },
+        () => {
+          // Upload completed successfully, now we can get the download URL
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            console.log("File available at", downloadURL);
+            setImageURL(downloadURL);
+            console.log("marioooo", imageURL);
+          });
+        }
+      );
+    };
+
+    if (image !== null) {
+      uploadImage();
+      setImage(null);
+    }
+  }, [image]);
+  return (
+    <View style={styles.container}>
+      <ScrollView>
+        {error === true && <Text style={styles.error}>{message}</Text>}
+
+        <InputImage
+          placeholder="Nome"
+          imageName="leaf"
+          onChangeText={(text) => setName(text)}
+          value={name}
+        />
+        <InputImageDescription
+          placeholder="Descricao"
+          imageName="leaf"
+          onChangeText={(text) => setDescription(text)}
+          value={description}
+        />
+        {imageURL != "" && (
+          <Image
+            source={{ uri: imageURL }}
+            style={{ width: 200, height: 200, borderRadius: 100 }}
+          />
+        )}
+        <View style={styles.buttonContainer}>
+          <Button
+            title="Escolha uma imagem da galeria"
+            color="#2F4F4F"
+            onPress={pickImage}
+          />
         </View>
-    );
+        <View style={styles.buttonContainer}>
+          <Button
+            title="Cadastrar"
+            color="#2F4F4F"
+            style={styles.byton}
+            onPress={() => addPlant() && navigation.navigate("Home")}
+          >
+            Cadastrar
+          </Button>
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button
+            title="Voltar"
+            color="#2F4F4F"
+            style={styles.byton}
+            onPress={() => navigation.navigate("Home")}
+          >
+            Voltar
+          </Button>
+        </View>
+      </ScrollView>
+    </View>
+  );
 }
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#3CB371',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    buttonContainer: {
-        marginTop: 20,
-        width: 300,
-        borderRadius: 20,
-        overflow: 'hidden',
-    },
-    description: {
-        height: 100,
-    }
+  container: {
+    flex: 1,
+    backgroundColor: "#3CB371",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonContainer: {
+    marginTop: 20,
+    width: 300,
+    borderRadius: 20,
+    overflow: "hidden",
+  },
 });
